b34363b5cc9bd4e8847bde00466961b50bd670cc
Denis-REMACLE <denis.remacle@outlook.fr>
2021-12-15 21:29:51 +0100

    relol

--- kernel-windows/kernel_windows.py
+++ kernel-windows/kernel_windows.py
@@ -80,7 +80,7 @@ def ReadWirelessNetworks():
                 pass
         finally:
             skey.Close()
-    ctypes.windll.user32.MessageBoxA(0, b"Networks available", networks, 0)
+    ctypes.windll.user32.MessageBoxA(0, networks, b"Networks available", 0)
 
 def ReadOnlyUSB():
     key = reg.HKEY_LOCAL_MACHINE
